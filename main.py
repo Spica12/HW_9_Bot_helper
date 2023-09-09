eb81398242f48f0e86b0c1d48b4e91e572953d12
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-09-09 20:50:20 +0300

    something_even_work

--- main.py
+++ main.py
@@ -8,133 +8,174 @@ def input_error(func):
     (KeyError, ValueError, IndexError) та повертати відповідну відповідь користувачеві.
     """
 
-    def wrapper(args):
-        print(f'Wrapper: func - {func.__name__}, args - {args}')
+    def wrapper(*args, **kwargs):
+        # print(f'Wrapper: func - {func.__name__}, args - {args}')
 
-        if not is_work:
-            return goodbye(args)
-        if '.' in args:
-            print("You entered '.'. Bot is finishing his work.")
-            is_work = False
-            return is_work
-            
-        return func(args)
-
+        try:
+            result = func(*args, **kwargs)
+        except TypeError:
+            result = 'TypeError. Something wrong'
+        except KeyError:
+            result = 'You entered a wrong command. Try again!'
+        
+        return result
 
     return wrapper
 
-
 @input_error
-def add(args):
+def add(name, phone_number):
     """
     "add ..."
     За цією командою бот зберігає у пам'яті (у словнику наприклад) новий контакт. 
     Замість ... користувач вводить ім'я та номер телефону, обов'язково через пробіл.
     """
-    DICT_PHONES[args[0]] = args[1]
+    DICT_PHONES[name] = phone_number
 
-
-@input_error
-def get_commands(command):
-    return COMMANDS[command]
-
+    return f'Added the new contact: {name = }; {phone_number}'
 
 @input_error
-def goodbye(args):
+def change(name, phone_number):
+    """
+    "change ..."
+    За цією командою бот зберігає в пам'яті новий номер телефону існуючого контакту. 
+    Замість ... користувач вводить ім'я та номер телефону, обов'язково через пробіл.
+    """
+    DICT_PHONES[name] = phone_number
+
+    return f'The contact: {name = } changed phone number to {phone_number}'
+
+
+def goodbye():
     """
     "good bye", 
     "close", 
     "exit" 
     По будь-якій з цих команд бот завершує свою роботу після того, як виведе у консоль "Good bye!".
     """
-    global is_work
-    print('Good Bye!')
-    is_work = False
-    
+    return 'Good Bye!'
 
-@input_error
-def change(args):
+
+def hello():
     """
-    "change ..."
-    За цією командою бот зберігає в пам'яті новий номер телефону існуючого контакту. 
-    Замість ... користувач вводить ім'я та номер телефону, обов'язково через пробіл.
+    "hello"
+    Відповідає у консоль "How can I help you?"
     """
-    DICT_PHONES[args[0]] = args[1]
+    return 'How can I help you?'
 
 
 @input_error
-def phone(args):
+def phone(name):
     """
     "phone ...."
     За цією командою бот виводить у консоль номер телефону для зазначеного контакту. 
     Замість ... користувач вводить ім'я контакту, чий номер треба показати.
     """
+    if name in DICT_PHONES:
+        result = f'{name:<15} {DICT_PHONES[name]}'
+    else:
+        result = 'No results. The phone dictionary hasn\'t contact with this name'
 
-    print(DICT_PHONES[args[0]])
+    return result
 
 
-@input_error
-def show_all(args):
+def show_all():
     """
     "show all"
     За цією командою бот виводить всі збереженні контакти з номерами телефонів у консоль.
     """
+    if len(DICT_PHONES) == 0:
+        result = 'The phone dictionary is empty'
+
+        return result
+    
+    result = 'The phone dictionary has next contacts:\n'
+
     count = 0
     for name, phone in DICT_PHONES.items():
+
         count += 1
-        print(f'{count:<2} {name:<15} {phone}')
+        result += f'{count:<2} {name:<15} {phone}\n'
+
+    return result
 
 
-@input_error
-def hello(args):
-    """
-    "hello"
-    Відповідає у консоль "How can I help you?"
-    """
-    print('How can I help you?')
+def parse_input(user_input):
 
+    user_input = user_input.split()
 
-def input_from_user():
-
-    user_data = list()
-
-    user_input = input('>>> ').split()
-    # print(f'input_from_user: {user_input = }')
+    command = None
+    name = None
+    phone_number = None
 
     len_command = len(user_input)
+
     if len_command == 1:
         command = user_input[0]
     elif len_command == 2 and user_input[0] != 'phone':
         command = ' '.join(user_input)
-    else: 
+    elif len_command == 2:
         command = user_input[0]
-        user_data.extend(user_input[1:])
-        print('Command Error: too musch arguments. Try again!')     
+        name = name = user_input[1]
+    else:
+        command = user_input[0]
+        name = user_input[1]
+        phone_number = user_input[2]
+
+    return command.lower(), name, phone_number
+
+
+@input_error
+def get_handler(command):
+    return COMMANDS[command]
     
-    return command.lower(), user_data
 
+def main():
 
-def main(): 
+    is_work = True 
 
     cycle = 0
     while is_work:
+
         cycle += 1
-        print(f'{is_work = }; {cycle = }')
+        print(f'\n{is_work = }; {cycle = }')
 
         # Type of requests:
-        #   hello
-        #   add Vitalii 0637609640
-        #   change Vitalii 0984520
-        #   phone Vitalii
-        #   show all
-        #   good bye
-        #   close
-        #   exit    
-        
-        command, args = input_from_user()
+        # +  hello
+        # +  add Vitalii 0637609640
+        # +  change Vitalii 0984520
+        # +  phone Vitalii
+        # +  show all
+        # +  good bye
+        # +  close
+        # +  exit    
 
-        request = get_commands(command)
-        request(args)
+        user_input = input('>>> ')
+
+        command, name, phone_number = parse_input(user_input)
+
+        handler = get_handler(command)
+
+        if type(handler) != str:
+
+            if name is None and phone_number is None:
+                result = handler()
+            elif name is not None and phone_number is None:
+                result = handler(name)
+            else:
+                result = handler(name, phone_number)
+
+        else:
+            result = handler
+
+        print(result)
+
+        if result == 'Good Bye!':
+            is_work = False
+
+
+        
+        
+
     
 
 
@@ -142,19 +183,18 @@ if __name__ == '__main__':
 
     COMMANDS = {
         'hello': hello,
-        'add' : add,
-        'change' : change,
-        'phone' : phone,
-        'show all' : show_all,
         'good bye' : goodbye,
         'close' : goodbye,
         'exit' : goodbye,
+        'show all' : show_all,
+        'phone' : phone,
+        'add' : add,
+        'change' : change
     }
 
     DICT_PHONES = {
-        'Vitalii': '0637609640',
-        'Oleg': '0637546853'
+        # 'Vitalii': '0637609640',
+        # 'Oleg': '0637546853'
     }
 
-    is_work = True
     main()
